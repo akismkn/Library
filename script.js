c5020d1fa181df31de3bffba86786dd5cc26fe6a
akismkn <xkxsnrg@gmail.com>
2024-03-16 01:33:14 +0200

    added style and script for the read/not read button inside the card.

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const myLibrary = [];
-
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -17,22 +15,42 @@ function updateTheLibraryContainer(newBook) {
     newDiv.className = "card";
     // title transfer to the card
     let text = document.createElement("p");
-    text.innerHTML = "<span style='font-weight: bold;'>Title</span><br>" + newBook.title;
+    text.innerHTML = "<span style='font-weight: bold;'>Title:</span><br>" + newBook.title;
     newDiv.appendChild(text);
     // author transfer to the card
     text = document.createElement("p");
-    text.innerHTML = "<span style='font-weight: bold;'>Author</span><br>" + newBook.author;
+    text.innerHTML = "<span style='font-weight: bold;'>Author:</span><br>" + newBook.author;
     newDiv.appendChild(text);
-    // pages transfer to the card
+    // number of pages transfer to the card
     text = document.createElement("p");
-    text.innerHTML = "<span style='font-weight: bold;'>Pages</span><br>" + newBook.pages;
+    text.innerHTML = "<span style='font-weight: bold;'>Pages:</span><br>" + newBook.pages;
     newDiv.appendChild(text);
+    // read or not read button transfer to the card
+    const readButton = document.createElement("button");
+    readButton.innerText = "Read";
+    readButton.className = "readButton";
+    newDiv.appendChild(readButton);
+    // PENDING TO READ/NOT-READ BUTTON UPDATE
+    readButton.addEventListener("click", () => {
+        if (readButton.innerText === "Read") {
+            readButton.innerText = "Not read";
+            readButton.classList.add("redColor");
+        } else {
+            readButton.innerText = "Read";
+            readButton.classList.remove("redColor");
+        }
+    });
+    // delete button transfer to the card
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete"
+    deleteButton.className = "deleteButton";
+    newDiv.appendChild(deleteButton);
     // transfer the cards inside the library (page)
     const library = document.querySelector(".library");
     library.appendChild(newDiv);
 }
 
-
+// adds the inputs into a new book
 const form = document.getElementById("addBookForm");
 form.addEventListener("submit", function () {
     const title = document.getElementById("title").value;
@@ -42,15 +60,7 @@ form.addEventListener("submit", function () {
     addBookToLibrary(title, author, pages);
 })
 
-
-
-
-
-
-
-
-
-// OPEN AND CLOSE DIALOG BUTTONS (+ / X)
+// OPEN AND CLOSE DIALOG - BUTTONS(+ / X)
 const dialog = document.querySelector("dialog");
 
 const showButton = document.querySelector(".addBookButton");
@@ -64,4 +74,3 @@ const closeButton = document.querySelector(".close");
 closeButton.addEventListener("click", () => {
     dialog.close();
 })
-
